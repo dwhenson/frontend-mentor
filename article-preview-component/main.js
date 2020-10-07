7385cba90a7ba67fa6d32ce427e6e77e7019c43d
dwhenson <dave.henson@gmail.com>
2020-10-07 20:50:09 +0300

    🚀 RELEASE: first draft working version

--- article-preview-component/main.js
+++ article-preview-component/main.js
@@ -1,18 +1,12 @@
 /* ==========  Variables  ========== */
 
-const avatarIcon = document.querySelector("#avatar-icon");
-const shareIcon = document.querySelector("#share-icon");
 const sharePanel = document.querySelector("#share");
-const avatarPanel = document.querySelector("#avatar");
 
 /* ==========  Functions  ========== */
 
 function clickHandler(event) {
-	if (event.target === avatarIcon) {
-		sharePanel.style.display = "flex";
-	} else if (event.target === shareIcon) {
-		sharePanel.style.display = "none";
-	}
+	if (!event.target.classList.contains("toggle-icon")) return;
+	sharePanel.classList.toggle("is-visible");
 }
 
 /* ==========  Inits and Event Listeners  ========== */
