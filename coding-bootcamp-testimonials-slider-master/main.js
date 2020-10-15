2465292606f7933a57dd1cbf9260e0dd2b1b878a
dwhenson <dave.henson@gmail.com>
2020-10-15 21:12:08 +0300

    ⚡ ADD: working javascript

--- coding-bootcamp-testimonials-slider-master/main.js
+++ coding-bootcamp-testimonials-slider-master/main.js
@@ -1,8 +1,24 @@
-const slide = document.querySelector(".carousel-slide");
+const right = document.querySelector(".right");
+const left = document.querySelector(".left");
 
-function sizeHandler() {
-	const params = slide.getBoundingClientRect();
-	console.log(params);
+function showLeft() {
+	left.style.left = "0";
+	right.style.left = "100%";
 }
 
-window.addEventListener("resize", sizeHandler);
+function showRight() {
+	left.style.left = "-100%";
+	right.style.left = "0";
+}
+
+function clickHandler(event) {
+	if (!event.target.closest(".button-container")) return;
+	if (event.target.closest(".next")) {
+		showRight();
+	}
+	if (event.target.closest(".previous")) {
+		showLeft();
+	}
+}
+
+document.addEventListener("click", clickHandler);
