ecc769d400057cb954737a6fa06001719a6061a6
dwhenson <dave.henson@gmail.com>
2020-10-15 17:44:18 +0300

    ⚡ ADD: javascript starter file and button elements

--- coding-bootcamp-testimonials-slider-master/main.js
+++ coding-bootcamp-testimonials-slider-master/main.js
@@ -1,1 +1,8 @@
-console.log("It works");
+const slide = document.querySelector(".carousel-slide");
+
+function sizeHandler() {
+	const params = slide.getBoundingClientRect();
+	console.log(params);
+}
+
+window.addEventListener("resize", sizeHandler);
