af1c0a80f00a0d52bc56903bfade226552321546
dwhenson <dave.henson@gmail.com>
2020-10-28 21:39:59 +0300

    ⚡ ADD: Javascript and form validation (Bouncer.js)

--- 10.  intro-component-signup-form/main.js
+++ 10.  intro-component-signup-form/main.js
@@ -1,1 +1,19 @@
-console.log("It works");
+/* ==========  Variables  ========== */
+const form = new Bouncer("form");
+
+/* ==========  Functions  ========== */
+// Show the error icon on error
+function showIcon(event) {
+	const icon = event.target.nextElementSibling;
+	icon.style.visibility = "visible";
+}
+
+// Hide the error icon on resolved error
+function hideIcon(event) {
+	const icon = event.target.nextElementSibling;
+	icon.style.visibility = "hidden";
+}
+
+/* ==========  Inits and Event Listeners  ========== */
+document.addEventListener("bouncerShowError", showIcon);
+document.addEventListener("bouncerRemoveError", hideIcon);
