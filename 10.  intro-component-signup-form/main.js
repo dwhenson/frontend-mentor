72fc2cceafbaabcf86853749945b9a6ac94ef258
dwhenson <dave.henson@gmail.com>
2020-10-29 13:48:03 +0300

    ✨IMPROVE: solution from feedback

--- 10.  intro-component-signup-form/main.js
+++ 10.  intro-component-signup-form/main.js
@@ -1,19 +1,22 @@
-/* ==========  Variables  ========== */
-const form = new Bouncer("form");
+// Avoid global scope
+(function () {
+	/* ==========  Variables  ========== */
+	const form = new Bouncer("form");
 
-/* ==========  Functions  ========== */
-// Show the error icon on error
-function showIcon(event) {
-	const icon = event.target.nextElementSibling;
-	icon.style.visibility = "visible";
-}
+	/* ==========  Functions  ========== */
+	// Show the error icon on error
+	function showIcon(event) {
+		const icon = event.target.nextElementSibling;
+		icon.style.visibility = "visible";
+	}
 
-// Hide the error icon on resolved error
-function hideIcon(event) {
-	const icon = event.target.nextElementSibling;
-	icon.style.visibility = "hidden";
-}
+	// Hide the error icon on resolved error
+	function hideIcon(event) {
+		const icon = event.target.nextElementSibling;
+		icon.style.visibility = "hidden";
+	}
 
-/* ==========  Inits and Event Listeners  ========== */
-document.addEventListener("bouncerShowError", showIcon);
-document.addEventListener("bouncerRemoveError", hideIcon);
+	/* ==========  Inits and Event Listeners  ========== */
+	document.addEventListener("bouncerShowError", showIcon);
+	document.addEventListener("bouncerRemoveError", hideIcon);
+})();
