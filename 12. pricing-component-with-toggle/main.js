9bf94317f7626d2f2552e6f2e59fc933f69d38b6
dwhenson <dave.henson@gmail.com>
2020-11-28 15:17:34 +0300

    🚀 RELEASE: first draft working solution

--- 12. pricing-component-with-toggle/main.js
+++ 12. pricing-component-with-toggle/main.js
@@ -1,7 +1,19 @@
 const toggle = document.querySelector("#toggle-element");
+const priceSwitches = Array.from(document.querySelectorAll(".switch"));
 
 function togglePrice(event) {
-	console.log(event.target.checked);
+	if (event.target.checked) {
+		priceSwitches.forEach((priceSwitch) => {
+			if (priceSwitch.classList.contains("annually")) return;
+			priceSwitch.classList.add("annually");
+		});
+	}
+	if (!event.target.checked) {
+		priceSwitches.forEach((priceSwitch) => {
+			if (!priceSwitch.classList.contains("annually")) return;
+			priceSwitch.classList.remove("annually");
+		});
+	}
 }
 
 toggle.addEventListener("change", togglePrice);
