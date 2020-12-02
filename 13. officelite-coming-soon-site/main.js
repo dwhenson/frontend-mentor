32be13ce4f589a86dd59118ab5f9a11431382749
dwhenson <dave.henson@gmail.com>
2020-12-02 16:58:28 +0300

    ⚡ ADD: sign-up page html and basic CSS

--- 13. officelite-coming-soon-site/main.js
+++ 13. officelite-coming-soon-site/main.js
@@ -1,1 +1,11 @@
 console.log("It works");
+
+
+ <!-- If you're choosing to make this date dynamic, set it to 30 days in the future from the moment the visitor views the page -->
+  Coming 4 Nov 2020
+
+  <!-- If you're choosing to make this timer dynamic, have it countdown from the date you set above -->
+  47 Days
+  07 Hours
+  56 Min
+  14 Sec
