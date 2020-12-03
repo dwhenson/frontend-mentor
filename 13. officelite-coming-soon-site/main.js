a7c9dd53e89cb2014fe14f018dc35c812fa41675
dwhenson <dave.henson@gmail.com>
2020-12-03 21:16:54 +0300

    ⚡ ADD: javascript for dynamic countdown

--- 13. officelite-coming-soon-site/main.js
+++ 13. officelite-coming-soon-site/main.js
@@ -1,11 +1,51 @@
-console.log("It works");
+/* ============ Variables ========== */
 
+// HTML elements to render values
+const today = document.querySelector("#date");
+const days = document.querySelector("#days");
+const hours = document.querySelector("#hours");
+const mins = document.querySelector("#mins");
+const secs = document.querySelector("#secs");
 
- <!-- If you're choosing to make this date dynamic, set it to 30 days in the future from the moment the visitor views the page -->
-  Coming 4 Nov 2020
+// Date now and 30 days from now
+const now = new Date();
+const plus30 = now.setDate(now.getDate() + 30);
 
-  <!-- If you're choosing to make this timer dynamic, have it countdown from the date you set above -->
-  47 Days
-  07 Hours
-  56 Min
-  14 Sec
+/* ==========  Functions  ========== */
+
+/**
+ * Renders today's date in long form to the HTML
+ */
+function renderToday() {
+	const date = now.getDate();
+	const month = now.toLocaleString("default", { month: "long" });
+	const year = now.getFullYear();
+	today.innerHTML = `${date} ${month} ${year}`;
+}
+
+/**
+ * Creates a new Date and minuses it from the time now plus 30 days
+ */
+function countdownTimer() {
+	const newTime = new Date();
+	const countdown = plus30 - newTime;
+
+	// Converts ms to human readable values
+	const day = Math.floor(countdown / 1000 / 60 / 60 / 24);
+	const hour = Math.floor(countdown / 1000 / 60 / 60) % 24;
+	const min = Math.floor(countdown / 1000 / 60) % 60;
+	const sec = Math.floor(countdown / 1000) % 60;
+
+	// Renders the human readable values to HTML
+	days.textContent = day.toString().padStart(2, "0");
+	hours.textContent = hour.toString().padStart(2, "0");
+	mins.textContent = min.toString().padStart(2, "0");
+	secs.textContent = sec.toString().padStart(2, "0");
+}
+
+/* ==========  Inits and Event Listeners  ========== */
+renderToday();
+
+setInterval(() => {
+	countdownTimer();
+}, 1000);
