300242179ad24c6c061609a4ae3b34d5e070a35c
dwhenson <dave.henson@gmail.com>
2021-01-26 21:18:50 +0300

    ✅ DONE: Compete Ping coming soon HTML, CSS and add JS

--- 11. ping-coming-soon-page/main.js
+++ 11. ping-coming-soon-page/main.js
@@ -1,1 +1,44 @@
-console.log("It works");
+/* =================== Variables ====================== */
+/* ==================================================== */
+
+const form = document.querySelector("form");
+const submitForm = document.querySelector("button");
+const emailField = document.querySelector("#email");
+const errorField = document.querySelector(".error");
+
+/* =================== Functions ====================== */
+/* ==================================================== */
+
+/**
+ * Check input for valid email on key down
+ */
+function validateEmailKeydown() {
+	if (!form.checkValidity()) {
+		errorField.textContent = `Please provide a valid email address`;
+		emailField.classList.add("error");
+	} else {
+		emailField.classList.remove("error");
+	}
+}
+
+/**
+ * Check input for valid email on form submission
+ */
+function validateEmailSubmission() {
+	if (!form.checkValidity() || !emailField.value) {
+		errorField.textContent = `Please provide a valid email address`;
+		emailField.classList.add("error");
+	} else {
+		form.submit();
+	}
+	emailField.addEventListener("keydown", validateEmailKeydown);
+}
+
+/* ============  Inits and Event Listeners  =========== */
+/* ==================================================== */
+
+submitForm.addEventListener("click", function (event) {
+	event.preventDefault();
+	form.setAttribute("novalidate", "");
+	validateEmailSubmission(event);
+});
