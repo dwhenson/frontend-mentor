2fd78cbb295cda100cf5d06686d4f4676bffced6
dwhenson <dave.henson@gmail.com>
2021-02-16 20:51:14 +0300

    ✅ DONE: First draft working solution coffeeroasters challenge

--- 16. coffeeroasters-subscription-site/js/main.js
+++ 16. coffeeroasters-subscription-site/js/main.js
@@ -1,12 +1,30 @@
-const modalNav = document.querySelector(".mobile-modal");
+(function () {
+	/* =================== Variables ====================== */
+	/* ==================================================== */
 
-function clickHandler(event) {
-	if (event.target.closest("#menu")) {
-		modalNav.style.display = "block";
-	}
-	if (event.target.closest("#close")) {
-		modalNav.style.display = "none";
-	}
-}
+	const modalNav = document.querySelector(".mobile-modal");
+	const link = modalNav.querySelector("a");
 
-document.addEventListener("click", clickHandler);
+	/* =================== Functions ====================== */
+	/* ==================================================== */
+
+	/**
+	 * Hide and show the mobile navigation menu
+	 * @param  {event} event The event object
+	 */
+	function clickHandler(event) {
+		if (event.target.closest("#menu")) {
+			modalNav.style.display = "block";
+			// Move focus to first link
+			link.focus();
+		}
+		if (event.target.closest("#close")) {
+			modalNav.style.display = "none";
+		}
+	}
+
+	/* ============  Inits and Event Listeners  =========== */
+	/* ==================================================== */
+
+	document.addEventListener("click", clickHandler);
+})();
