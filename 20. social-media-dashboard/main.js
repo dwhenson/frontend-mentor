489b1ea92112489e612735e0ea058a2a4f8a7276
dwhenson <dave.henson@gmail.com>
2021-03-01 08:52:32 +0300

    📦 ADD: Add function comments

--- 20. social-media-dashboard/main.js
+++ 20. social-media-dashboard/main.js
@@ -1,6 +1,9 @@
 const checkbox = document.querySelector("#dark-mode");
 const root = document.querySelector("html");
 
+/**
+ * Adds/removes a class based on checkbox status
+ */
 function changeHandler() {
 	if (checkbox.checked) {
 		root.classList.add("dark-theme");
