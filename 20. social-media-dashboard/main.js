75656b6d882b55a60e292c05830bfb1cf4a2bd14
dwhenson <dave.henson@gmail.com>
2021-03-01 21:22:07 +0300

    📦 ADD: Working toggle switch styles and JS functionality

--- 20. social-media-dashboard/main.js
+++ 20. social-media-dashboard/main.js
@@ -1,15 +1,18 @@
+const toggle = document.querySelector(".toggle");
 const checkbox = document.querySelector("#dark-mode");
 const root = document.querySelector("html");
 
 /**
  * Adds/removes a class based on checkbox status
  */
-function changeHandler() {
-	if (checkbox.checked) {
+function changeHandler(event) {
+	if (event.target.checked) {
 		root.classList.add("dark-theme");
 	} else {
 		root.classList.remove("dark-theme");
 	}
 }
 
+// Remove hidden attribute only if JS loads
+toggle.removeAttribute("hidden");
 document.addEventListener("change", changeHandler);
