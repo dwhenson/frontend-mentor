3c547d5f9aa186f0aa122437d7c005a921170c66
dwhenson <dave.henson@gmail.com>
2021-03-05 16:27:00 +0300

    📦 ADD: Basic working JS to overwrite system color theme

--- 20. social-media-dashboard/main.js
+++ 20. social-media-dashboard/main.js
@@ -1,9 +1,17 @@
-const toggle = document.querySelector(".toggle");
 const root = document.querySelector("html");
+const toggle = document.querySelector(".toggle");
+const checkbox = document.querySelector("#toggle-element");
 
-/**
- * Adds/removes a class based on checkbox status
- */
+let state;
+
+function getState(propKey, element = document.documentElement) {
+	let response = getComputedStyle(element).getPropertyValue(propKey);
+	if (response.length) {
+		response = response.replace(/"/g, "").trim();
+	}
+	state = response;
+}
+
 function changeHandler(event) {
 	if (event.target.checked) {
 		root.classList.add("dark-theme");
@@ -12,6 +20,18 @@ function changeHandler(event) {
 	}
 }
 
+function updateCSS() {
+	if (state === "dark") {
+		checkbox.click();
+		root.classList.add("dark-theme");
+	}
+	if (state === "light") {
+		root.classList.remove("dark-theme");
+	}
+}
+
 // Remove hidden attribute only if JS loads
 toggle.removeAttribute("hidden");
+getState("--color-mode", root);
+updateCSS();
 document.addEventListener("change", changeHandler);
