f1a649577989b3cf614f0ca992bfce6a080fdc18
dwhenson <dave.henson@gmail.com>
2021-03-08 14:39:28 +0300

    ⚡ IMPROVE: Class names and JS hooks

--- 22. interactive-pricing-component/main.js
+++ 22. interactive-pricing-component/main.js
@@ -1,21 +1,21 @@
-const values = [
+const plans = [
 	{ views: "10K", price: 8, discount: 6 },
 	{ views: "50K", price: 12, discount: 9 },
 	{ views: "100K", price: 16, discount: 12 },
 	{ views: "500K", price: 24, discount: 18 },
 	{ views: "1M", price: 36, discount: 27 },
 ];
-const priceSlider = document.querySelector("#price-slider");
+const planSection = document.querySelector("#plans");
 const table = document.querySelector("#table");
 const input = document.querySelector("#input");
 const views = document.querySelector("#views");
 const price = document.querySelector("#price");
 
 input.oninput = function () {
-	views.innerHTML = values[this.value].views;
-	price.innerHTML = values[this.value].price;
+	views.innerHTML = plans[this.value].views;
+	price.innerHTML = plans[this.value].price;
 };
 
-// table.setAttribute("hidden", "true");
-priceSlider.removeAttribute("hidden");
+table.setAttribute("hidden", "true");
+planSection.removeAttribute("hidden");
 input.oninput();
