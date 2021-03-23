9d4ec4a2c71f74cd3c64ddf7afcd408081f79360
dwhenson <dave.henson@gmail.com>
2021-03-23 17:42:02 +0300

    📦 ADD: Basic variables for DOM updating

--- 26. crowdfunding-product-page/js/app.js
+++ 26. crowdfunding-product-page/js/app.js
@@ -1,3 +1,10 @@
-const updateBarWidth = () => {
-	rootStyles.setProperty("--width-bar", `${(priceRange.value * 100) / priceRange.max}%`);
-};
+const root = document.querySelector(":root");
+const totalUsd = document.querySelector("#total-usd").textContent;
+const totalUsdNumber = parseFloat(totalUsd.replace(/,/g, ""));
+const totalBackers = document.querySelector("#total-backers").textContent;
+
+function updateProgress() {
+	root.style.setProperty("--width-bar", `${(totalUsdNumber * 100) / 100000}%`);
+}
+
+updateProgress();
