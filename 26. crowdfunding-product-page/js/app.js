419c5570556f62b13869abc9fb0a1dbd60f47b12
dwhenson <dave.henson@gmail.com>
2021-03-24 15:14:34 +0300

    📖 DOC: Clean up comments and remove unused variables

--- 26. crowdfunding-product-page/js/app.js
+++ 26. crowdfunding-product-page/js/app.js
@@ -1,14 +1,10 @@
 const root = document.querySelector(":root");
 const totalUsd = document.querySelector("#total-usd");
-// let totalUsdNumber = Number(totalUsd.textContent.replace(/,/g, ""));
 const totalBackers = document.querySelector("#total-backers");
-// let totalBackersNumber = Number(totalBackers.textContent.replace(/,/g, ""));
 const pledgeButtons = document.querySelectorAll(".continue");
 const remainingModal = document.querySelectorAll(".remaining-modal");
-const remainingPage = document.querySelectorAll(".remaining-page");
 const pledgeAmounts = document.querySelectorAll("input[name='commit']");
 const remaining = document.querySelectorAll("[data-type]");
-// const mainDialog = document.querySelector("#dialogMain");
 
 let pledged = 89914;
 let backers = 5007;
@@ -29,7 +25,7 @@ function disableNotAvailible(remainder) {
 	button.disabled = true;
 	if (remainder.classList.contains("remaining-modal")) {
 		const input = remainder.closest(".box").querySelector("input");
-		input.setAttribute("disabled", "disabled");
+		input.setAttribute("disabled", "true");
 	}
 }
 
@@ -65,35 +61,3 @@ function clickHandler(event) {
 
 renderHTML();
 document.addEventListener("click", clickHandler);
-
-// remainingPage.forEach(function (page, index) {
-// 	checkStock(index);
-// });
-// remainingModal.forEach(function (page, index) {
-// 	checkStock(index);
-// });
-
-// function checkStock(index) {
-// 	const page =
-// 	const modal = Number(remainingModal[index].textContent) - 1;
-// 	// if (page <= 0) {
-// 	// 	remainingPage[index].closest(".box").classList.add("js-out-of-stock");
-// 	// 	const button = remainingPage[index].closest(".box").querySelector(".cta");
-// 	// 	button.textContent = `Out of stock`;
-// 	// 	button.disabled = true;
-// 	// 	page.textContent = 0;
-// 	// } else {
-// 	// 	page.textContent = `${Number(remainingPage[index].textContent) - 1}`;
-// 	// }
-
-// 	// if (modal <= 0) {
-// 	// 	remainingModal[index].closest(".box").classList.add("js-out-of-stock");
-// 	// 	const radioButton = remainingModal[index]
-// 	// 		.closest(".box")
-// 	// 		.querySelector("input[name='commit']");
-// 	// 	radioButton.disabled = true;
-// 	// 	modal.textContent = 0;
-// 	// } else {
-// 	// 	modal.textContent = `${Number(remainingModal[index].textContent) - 1}`;
-// 	// }
-// }
