c0f0489a221c90d6c396274e9174c3380047c1aa
dwhenson <dave.henson@gmail.com>
2021-03-24 13:35:02 +0300

    📦 ADD: Fully working DOM updates

--- 26. crowdfunding-product-page/js/app.js
+++ 26. crowdfunding-product-page/js/app.js
@@ -1,44 +1,70 @@
 const root = document.querySelector(":root");
 const totalUsd = document.querySelector("#total-usd");
-let totalUsdNumber = Number(totalUsd.textContent.replace(/,/g, ""));
+// let totalUsdNumber = Number(totalUsd.textContent.replace(/,/g, ""));
 const totalBackers = document.querySelector("#total-backers");
-let totalBackersNumber = Number(totalBackers.textContent.replace(/,/g, ""));
+// let totalBackersNumber = Number(totalBackers.textContent.replace(/,/g, ""));
 const pledgeButtons = document.querySelectorAll(".continue");
 const remainingModal = document.querySelectorAll(".remaining-modal");
 const remainingPage = document.querySelectorAll(".remaining-page");
 const pledgeAmounts = document.querySelectorAll("input[name='commit']");
-const mainDialog = document.querySelector("#dialogMain");
+const remaining = document.querySelectorAll("[data-type]");
+// const mainDialog = document.querySelector("#dialogMain");
+
+let pledged = 89914;
+let backers = 5007;
+const stock = {
+	bamboo: 101,
+	black: 64,
+	mahogany: 0,
+};
 
 function updateProgress() {
-	root.style.setProperty("--width-bar", `${(totalUsdNumber * 100) / 100000}%`);
+	root.style.setProperty("--width-bar", `${(pledged * 100) / 100000}%`);
 }
 
-function renderHTML(index) {
-	totalUsd.textContent = `${totalUsdNumber.toLocaleString("en")}`;
-	totalBackers.textContent = `${totalBackersNumber.toLocaleString("en")}`;
-	remainingPage[index].textContent = `${
-		Number(remainingPage[index].textContent) - 1
-	}`;
-	remainingModal[index].textContent = `${
-		Number(remainingModal[index].textContent) - 1
-	}`;
+function disableNotAvailible(remainder) {
+	remainder.closest(".box").classList.add("js-out-of-stock");
+	const button = remainder.closest(".box").querySelector(".cta");
+	button.textContent = `Out of stock`;
+	button.disabled = true;
+	if (remainder.classList.contains("remaining-modal")) {
+		const input = remainder.closest(".box").querySelector("input");
+		input.setAttribute("disabled", "disabled");
+	}
+}
+
+function renderHTML() {
+	totalUsd.textContent = `${pledged.toLocaleString("en")}`;
+	totalBackers.textContent = `${backers.toLocaleString("en")}`;
+	remaining.forEach(function (remainder) {
+		remainder.textContent = stock[remainder.dataset.type];
+		if (stock[remainder.dataset.type] <= 0) {
+			remainder.textContent = 0;
+			disableNotAvailible(remainder);
+		}
+	});
 	updateProgress();
+}
+
+function updateStock(index) {
+	stock[remainingModal[index].dataset.type] -= 1;
+	renderHTML(index);
 }
 
 function clickHandler(event) {
 	if (!event.target.closest(".continue")) return;
 	pledgeButtons.forEach(function (pledge, index) {
 		if (event.target === pledge) {
-			totalUsdNumber += Number(pledgeAmounts[index].value);
-			totalBackersNumber += 1;
-			// checkStock(index);
+			pledged += Number(pledgeAmounts[index].value);
+			backers += 1;
+			updateStock(index);
 			renderHTML(index);
 		}
 	});
 }
 
-updateProgress();
-mainDialog.addEventListener("click", clickHandler);
+renderHTML();
+document.addEventListener("click", clickHandler);
 
 // remainingPage.forEach(function (page, index) {
 // 	checkStock(index);
